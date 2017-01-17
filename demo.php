6d39af608dfc5ef738efd90bad5f2c6951d2569d
jackyzhang <852566802@qq.com>
2017-01-17 11:44:44 +0800

    update

--- demo.php
+++ demo.php
@@ -7,6 +7,8 @@ $pheanstalk = new Pheanstalk('127.0.0.1');
 
 
 $pheanstalk->useTube('testtube')->put('job playload goes here\h');
+$pheanstalk->useTube('testtube')->put('zz\h');
+$pheanstalk->useTube('testtube')->put('jj\h');
 
 $job = $pheanstalk->watch('testtube')->ignore('default')->reserve();
 
