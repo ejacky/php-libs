2d875359979da751239d2c3963511351e805ee01
zhangjianfei <zhangjianfei@b.360.cn>
2017-11-17 18:02:49 +0800

    update

--- my.php
+++ my.php
@@ -11,19 +11,29 @@ class My
     public function __construct()
     {
         $this->faker = Faker\Factory::create();
-        $this->red = $this->create_redis();
+        $this->red_cli = $this->createRedis();
 
-        $this->init_log();
+        $this->init();
     }
 
-    private function init_log()
+    private function init()
+    {
+        $this->initConfig();
+        $this->initLog();
+    }
+
+    private function initConfig()
+    {
+        date_default_timezone_set('Asia/Shanghai');
+    }
+    private function initLog()
     {
         $this->log = new Monolog\Logger('my-log');
         $this->log->pushHandler(new Monolog\Handler\StreamHandler(__DIR__.'/my.log', Monolog\Logger::DEBUG));
         $this->log->pushHandler(new Monolog\Handler\FirePHPHandler());
     }
 
-    public function create_redis()
+    public function createRedis()
     {
         if (!$this->red) {
             $this->red = new Predis\Client();
@@ -31,98 +41,107 @@ class My
         return $this->red;
     }
 
-    public function parse_time($id)
+    public function parseTime($id)
     {
         $time = intval($id) * 60;
         $time_str = date('Y-m-d H:i', $time);
         echo $time_str;
     }
 
-    public function record_policy_log($mid, $data)
+    public function cleanPolicyCache()
     {
-        $record_time = $this->faker->unixTime();
-        $data['time'] = $record_time;
-        $stat_slot_key = floor($record_time / 60);
-        $exist_key = $this->red->hexists('client_policy_stats', $stat_slot_key);
-        $this->red->pipeline(function ($pipe) use($stat_slot_key, $mid, $data, $exist_key) {
-            $pipe->hset('client_policy', $mid, json_encode($data));
-            $pipe->hincrby('client_policy_stats', $stat_slot_key, 1);
-            if ($exist_key == 0) {
-                $pipe->lpush('client_policy_time_series', $stat_slot_key);
-            }
-
-            $data['mid'] = $mid;
-            $pipe->lpush('client_policy_fetch_data_' . $stat_slot_key, json_encode($data));
-
-        });
-    }
-
-    public function clean_policy_cache()
-    {
-        $expire_time = floor((time() - 1 * 24 * 60 * 60) / 60);
+        $expire_time = floor((time() - 60) / 60);
         while (true) {
-            $len = $this->red->llen('client_policy_time_series');
-            if ($len <= 0) {
-                echo "为空";
+            $len = $this->red_cli->llen('client_policy_time_series');
+            if ($len <= 0)
                 break;
-            }
 
-            $time_slot_key = $this->red->lindex('client_policy_time_series', $len - 1);
+            $time_slot_key = $this->red_cli->lindex('client_policy_time_series', $len - 1);
             if ($time_slot_key < $expire_time) {
-                try {
-                    $this->red->pipeline(function ($pipe) use($time_slot_key) {
-                        $pipe->rpop('client_policy_time_series');
-                        $pipe->hdel('client_policy_stats',  array($time_slot_key));
-                        $pipe->del(array('client_policy_fetch_data_' . $time_slot_key));
-                    });
-                } catch (\Exception $e) {
-                    echo $e->getMessage();
-                }
-            } else {
-                echo "已清空";
-                break;
+                $this->red_cli->pipeline(function ($pipe) use($time_slot_key) {
+                    $pipe->rpop('client_policy_time_series');
+                    $pipe->hdel('client_policy_stats',  array($time_slot_key));
+                    $pipe->del(array('client_policy_fetch_data_' . $time_slot_key));
+                });
             }
         }
     }
 
-    public function test_faker()
+    public function cleanTest()
     {
-        $len = $this->red->llen('client_policy_time_series');
-        $time_slot_key = $this->red->lindex('client_policy_time_series', $len - 1);
-        echo $time_slot_key;
-        //echo json_encode($this->fuck_test());
-//        echo time();
-//        echo PHP_EOL;
-//        echo $this->faker->unixTime();
-//        echo PHP_EOL;
-//        echo $this->faker->md5;
+        $expire_time = floor((time() - 0.1  * 60 * 60) / 60);
+        //$this->red_cli->hdel('client_policy_stats', array('11216321'));
+        $this->red_cli->del(array('client_policy_stats'));
+//        while (true) {
+//            $len = $this->red_cli->llen('client_policy_time_series');
+//            if ($len <= 0)
+//                break;
+//
+//            //$time_slot_key = $this->red_cli->lindex('client_policy_time_series', $len - 1);
+//
+//            if ($time_slot_key < $expire_time) {
+//                //$this->red_cli->hdel('client_policy_stats',  array($time_slot_key));
+//                $this->red_cli->del(array('client_policy_fetch_data_' . $time_slot_key));
+//            }
+//        }
     }
 
-    public function my_test()
+
+    public function recordPolicyLog($mid, $data, $stat_slot_key)
     {
-        //$this->red->set('laozhang', 'zzzzzzzzzzzzzzzzzzz');
-        //$this->red->del(array('laozhang'));
-        //$this->red->hdel('');
+        $record_time = time();
+        $data['time'] = $record_time;
+        //$stat_slot_key = floor($record_time / 60);
+        $exist_key = $this->red_cli->hexists('client_policy_stats', $stat_slot_key);
+        $this->red_cli->pipeline(function ($pipe) use($stat_slot_key, $mid, $data, $exist_key) {
+            $pipe->hset('client_policy', $mid, json_encode($data));
+            $pipe->hincrby('client_policy_stats', $stat_slot_key, 1);
+            if ($exist_key == 0) {
+                $pipe->lpush('client_policy_time_series', array($stat_slot_key));
+            }
+
+            $data['mid'] = $mid;
+            $pipe->lpush('client_policy_fetch_data_' . $stat_slot_key, array(json_encode($data)));
+        });
     }
 
-    public function fuck_test()
+    public function generateFakerData()
     {
-        $this->red->lpush();
-        $mid = $this->faker->md5;
+        $mid = $this->generateRandomString(25);
+        $policy_name = $this->generateRandomString(3);
         $data = array(
-            'time' => $this->faker->unixTime,
+            'time' => time(),
             'mid' => $mid,
             'server_info' => array(
-                $this->faker->word => array('conf_ver' => $this->faker->unixTime())
+                $policy_name => array('conf_ver' => time())
             )
         );
-        $this->record_policy_log($mid, $data);
+
+        $i = 0;
+        $stat_slot_key = floor((time() - 31 * 24 * 60 * 60) / 60);
+        while ($i < 3) {
+            $this->recordPolicyLog($mid, $data, $stat_slot_key);
+            $stat_slot_key++;
+            $i++;
+        }
+    }
+
+    function generateRandomString($length = 10) {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
     }
 }
 
 $my = new My();
-$my->parse_time('25180324');
+$my->parseTime('25180324');
 echo date('Y-m-d H:i');
-//$my->clean_policy_cache();
+//$my->generateFakerData();
+$my->cleanPolicyCache();
+//$my->cleanTest();
 //$my->record_policy_log();
 //$my->my_test();
