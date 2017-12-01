b9d4e08c2e1de9eda842e1a062c98703058065ba
zhangjianfei <ejacky001@gmail.com>
2017-12-01 23:00:43 +0800

    update

--- pheanstalk_demo.php
+++ pheanstalk_demo.php
@@ -4,7 +4,24 @@ include "vendor/autoload.php";
 use Pheanstalk\Pheanstalk;
 
 $pheanstalk = new Pheanstalk('10.98.2.115');
+$red_cli = new \Predis\Client('tcp://10.98.2.115:6379');
 $faker = Faker\Factory::create();
+//
+//if (class_exists('Tideways\Profiler')) {
+//    echo "hello world";
+//    \Tideways\Profiler::start(array(
+//        'api_key' => 'YOUR API KEY',
+//        'sample_rate' => 25,
+//        'framework' => 'OPTIONAL; See below',
+//    ));
+//
+//
+//}
+//
+//
+//if (class_exists('Tideways\Profiler')) {
+//    \Tideways\Profiler::stop();
+//}
 
 //$pheanstalk->useTube('testtube')->put('job playload goes here\h');
 //$pheanstalk->useTube('testtube')->put('zz\h');
@@ -13,16 +30,20 @@ $faker = Faker\Factory::create();
 //$job = $pheanstalk->watch('testtube')->ignore('default')->reserve();
 
 //$i = 0;
-while (true) {
-    $msg        = array(
-        'msg_type' => 'hostinfo',
-        'mac'    => $faker->macAddress,
-        'mid'   => $faker->md5,
-        's_ip'  => $faker->ipv4
 
-    );
-    $pheanstalk->useTube('work_queue_nac_device_info')->put(json_encode($msg));
-    //$i++;
+
+
+
+function foo() {
+    echo "hello";
+}
+$i = 0;
+while ($i < 5) {
+    $content = file_get_contents('tmp/nac_deviceinfo.json');
+    $cont_arr = json_decode($content, true);
+    $cont_arr['mac'] = $faker->macAddress;
+    $pheanstalk->useTube('work_queue_nac_device_info')->put(json_encode($cont_arr));
+    $i++;
 }
 
 exit;
